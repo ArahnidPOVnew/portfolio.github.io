71db3cbf0502b615695af8ab1e0eda1a46fb9a6c
Ponomarenko Oleksandr <antares111182@gmail.com>
2017-04-09 23:30:11 +0300

    Змінив дизайн, зробив адаптив

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,37 +1,24 @@
-function diplay_hide (block_skills)
 
-{ 
-    if ($(block_skills).css('display') == 'none') 
-        { 
-            $(block_skills).animate({height: 'show'}, 500); 
-        } 
-    else 
-        {     
-            $(block_skills).animate({height: 'hide'}, 500); 
-        }}
-
-        
-		
 $(document).ready(function() {
  $(".site__show1").css("display", "none");
- $(".site__show1").fadeIn(2000);
+ $(".site__show1").fadeIn(3000);
  });
  
  $(document).ready(function() {
  $(".site__show2").css("display", "none");
- $(".site__show2").fadeIn(4000);
+ $(".site__show2").fadeIn(3000);
  });
  
  $(document).ready(function() {
  $(".site__show3").css("display", "none");
- $(".site__show3").fadeIn(5000);
+ $(".site__show3").fadeIn(3000);
  });
  
  $(document).ready(function() {
  $(".site__show4").css("display", "none");
- $(".site__show4").fadeIn(6000);
+ $(".site__show4").fadeIn(3000);
  });
 $(document).ready(function() {
  $("header").css("display", "none");
- $("header").fadeIn(2000);
+ $("header").fadeIn(3000);
  });
